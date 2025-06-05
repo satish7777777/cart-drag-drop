2044aaa627fab09dfb1876befb527bbd152a684e
Satish Kumar <satishjmdav@gmail.com>
2025-06-05 11:33:44 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    domains: ['images.unsplash.com', 'cdn.pixabay.com','picsum.photos','v.wpimg.pl', 'images.pexels.com']
+  }
 };
 
 export default nextConfig;
--- src/app/components/image.tsx
+++ src/app/components/image.tsx
@@ -1,12 +1,30 @@
+// 'use client';
+
+// export default function ImageComponent() {
+//   return (
+//     <div
+//       className="w-full h-full bg-cover bg-center rounded-xl"
+//       style={{
+//         backgroundImage: `url("https://v.wpimg.pl/ZTZkNzI0dTU7CThJZkt4IHhRbBMgEnZ2L0l0WGYBaGxqE2EcIFw_Jj8bIVQuQi8kOxw-VDlcdTUqAmEMeB8-PSkbIhswHz85OA4qVSxSaGJjXyhPZAk4Mm9GekJ-U3c1Y1p2V34JOTFsD3lIfQY7Y3gW")`,
+//       }}
+//     ></div>
+//   );
+// }
+
 'use client';
+
+import Image from 'next/image';
 
 export default function ImageComponent() {
   return (
-    <div
-      className="w-full h-full bg-cover bg-center rounded-xl"
-      style={{
-        backgroundImage: `url("https://v.wpimg.pl/ZTZkNzI0dTU7CThJZkt4IHhRbBMgEnZ2L0l0WGYBaGxqE2EcIFw_Jj8bIVQuQi8kOxw-VDlcdTUqAmEMeB8-PSkbIhswHz85OA4qVSxSaGJjXyhPZAk4Mm9GekJ-U3c1Y1p2V34JOTFsD3lIfQY7Y3gW")`,
-      }}
-    ></div>
+    <div className="w-full h-full relative rounded-xl overflow-hidden">
+      <Image
+        src="https://v.wpimg.pl/ZTZkNzI0dTU7CThJZkt4IHhRbBMgEnZ2L0l0WGYBaGxqE2EcIFw_Jj8bIVQuQi8kOxw-VDlcdTUqAmEMeB8-PSkbIhswHz85OA4qVSxSaGJjXyhPZAk4Mm9GekJ-U3c1Y1p2V34JOTFsD3lIfQY7Y3gW"
+        alt="Map Background"
+        fill
+        className="object-cover"
+        priority
+      />
+    </div>
   );
 }
