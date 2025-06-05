35e68e1ac37c36ae15b352d3a17a183c3165b9d5
Satish Kumar Gupta <99494605+satish7777777@users.noreply.github.com>
2025-06-05 14:35:15 +0530

    Update image.tsx

--- src/app/components/image.tsx
+++ src/app/components/image.tsx
@@ -1,16 +1,3 @@
-// 'use client';
-
-// export default function ImageComponent() {
-//   return (
-//     <div
-//       className="w-full h-full bg-cover bg-center rounded-xl"
-//       style={{
-//         backgroundImage: `url("https://v.wpimg.pl/ZTZkNzI0dTU7CThJZkt4IHhRbBMgEnZ2L0l0WGYBaGxqE2EcIFw_Jj8bIVQuQi8kOxw-VDlcdTUqAmEMeB8-PSkbIhswHz85OA4qVSxSaGJjXyhPZAk4Mm9GekJ-U3c1Y1p2V34JOTFsD3lIfQY7Y3gW")`,
-//       }}
-//     ></div>
-//   );
-// }
-
 'use client';
 
 import Image from 'next/image';
